49a3b3a91306c167473d2d07d2f041f381e47ee8
Anton Shevchuk <ashevchuk22@gmail.com>
2023-04-05 21:51:45 +1100

    At least it compiles

--- unsymmetrics/unsymmetrics.cpp
+++ unsymmetrics/unsymmetrics.cpp
@@ -24,11 +24,14 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
+    
+    /*
     IMGUI_CHECKVERSION();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGui::StyleColorsDark();
- 
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+    */
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
